8a85a64396fe0cc07bb6647ecd8014c3f4511372
thomas76943-Alienware <thomasrmoore@btinternet.com>
2018-01-24 21:28:08 +0000

    Test5

--- BenchmarkingUtility/Algorithms.cs
+++ BenchmarkingUtility/Algorithms.cs
@@ -15,7 +15,7 @@ namespace BenchmarkingUtility
         //File Paths for Algorithms
         public static string[] cpupaths = Directory.GetFiles(Path.Combine(Application.StartupPath, @"CPUAlgorithms\"));
         public static string[] gpupaths = Directory.GetFiles(Path.Combine(Application.StartupPath, @"GPUAlgorithms\"));
-
+        ///
         /// <summary>
         /// Runs and Retreives output of Python scripts
         /// </summary>
